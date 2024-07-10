b26bbf3d90bbde081590e02e73c1ce68be051782
Dardouri <dardourisenpay@gmail.com>
2024-07-10 12:06:48 +0200

    Ajoute traduction francais , anglais

---
+++ src/Controller/LangueController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LangueController extends AbstractController
+{
+
+    #[Route('/langue/anglais', name: 'app_langue_anglais')]
+    public function anglais(Request $request): Response
+    {
+        $request->getSession()->set('_locale', 'en');
+
+        return $this->redirect($request->server->get('HTTP_REFERER'));
+    }
+
+    #[Route('/langue/francais', name: 'app_langue_francais')]
+    public function francais(Request $request): Response
+    {
+        $request->getSession()->set('_locale', 'fr');
+
+        return $this->redirect($request->server->get('HTTP_REFERER'));
+    }
+}
